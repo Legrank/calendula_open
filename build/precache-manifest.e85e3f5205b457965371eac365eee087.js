f567a1b8e80d2c1992584327af66a5e3fbfea142
legrank <flyu.gyu@yandex.ru>
2020-10-11 07:02:35 +0300

    + build

---
+++ build/precache-manifest.e85e3f5205b457965371eac365eee087.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c3b657c80421d4232a9038541a1cf965",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7433d6d44a98d7194113",
+    "url": "/static/css/main.1b49a39a.chunk.css"
+  },
+  {
+    "revision": "58f3a4657315044b7622",
+    "url": "/static/js/2.88f4288f.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.88f4288f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7433d6d44a98d7194113",
+    "url": "/static/js/main.4735e5e8.chunk.js"
+  },
+  {
+    "revision": "041ad5a1712310651593",
+    "url": "/static/js/runtime-main.eb25f102.js"
+  },
+  {
+    "revision": "d7a3146694714fc4551c46d1eb4078f0",
+    "url": "/static/media/Rectangle 212.d7a31466.png"
+  },
+  {
+    "revision": "aeb25612af044893d5b1c4a671436748",
+    "url": "/static/media/Rectangle 220.aeb25612.png"
+  },
+  {
+    "revision": "aa15220915c67cd7967173cda287cf2e",
+    "url": "/static/media/Rectangle 221.aa152209.png"
+  },
+  {
+    "revision": "2044bab302bbdc5d7b4aaa200d668eed",
+    "url": "/static/media/Rectangle 222.2044bab3.png"
+  },
+  {
+    "revision": "b97d1bf5758ad9cf2980774bfef0646c",
+    "url": "/static/media/Rectangle 223.b97d1bf5.png"
+  },
+  {
+    "revision": "d39c31296549a6249cfde767c9d00a8c",
+    "url": "/static/media/Rectangle 224.d39c3129.png"
+  },
+  {
+    "revision": "605249779a602aac7bd696ef5b448031",
+    "url": "/static/media/Rectangle 225.60524977.png"
+  },
+  {
+    "revision": "96cc6720717a3c50b24e4f0b18e4eb58",
+    "url": "/static/media/Rectangle 226.96cc6720.png"
+  },
+  {
+    "revision": "e2832b52599a798e3d044f0e1734602c",
+    "url": "/static/media/Rectangle 227.e2832b52.png"
+  }
+]);
